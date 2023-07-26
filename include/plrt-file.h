8a3723d868134634182e6891926a8b15f14c98be
pocketlinux32 <pocketlinux32@gmail.com>
2023-07-26 04:48:08 -0500

    stuff

---
+++ include/plrt-file.h
@@ -0,0 +1,28 @@
+/********************************************\
+ pl-rt, v0.01
+ (c) 2023 pocketlinux32, Under MPL v2.0
+ plrt-file.h: File management module header
+\********************************************/
+
+#pragma once
+#include <plrt-memory.h>
+
+typedef struct plfile plfile_t;
+
+plfile_t* plFOpen(char* filename, char* mode, plmt_t* mt);
+plfile_t* plFToP(FILE* pointer, plmt_t* mt);
+int plFClose(plfile_t* ptr);
+
+size_t plFRead(plptr_t ptr, plfile_t* stream);
+size_t plFWrite(plptr_t ptr, plfile_t* stream);
+
+int plFPutC(byte_t ch, plfile_t* stream);
+int plFGetC(plfile_t* stream);
+
+int plFPuts(plstring_t string, plfile_t* stream);
+int plFGets(plstring_t string, plfile_t* stream);
+
+int plFSeek(plfile_t* stream, long int offset, int whence);
+size_t plFTell(plfile_t* stream);
+
+int plFPToFile(string_t filename, plfile_t* stream);
---
+++ include/plrt-types.h
@@ -0,0 +1,48 @@
+/**************************************\
+ pl-rt, v0.01
+ (c) 2023 pocketlinux32, Under MPL v2.0
+ plrt-error.h: Error handling module
+\**************************************/
+
+#pragma once
+#ifndef __cplusplus
+#include <stdbool.h>
+#include <stdint.h>
+#endif
+
+#define PLRT_VERSION "0.01"
+#define PLRT_API_VER 0
+#define PLRT_FEATURELVL 1
+#define PLRT_PATCHLVL 0
+#define UINT_SHIFT_LIMIT 63
+
+typedef uint8_t byte_t;
+typedef void* memptr_t;
+
+typedef struct plptr {
+	memptr_t pointer;
+	size_t size;
+} plptr_t;
+
+typedef enum plrtret {
+	PLRT_ERROR = ((uint64_t)1 << UINT_SHIFT_LIMIT),
+
+	PLRT_MEMORY = ((uint64_t)1 << (UINT_SHIFT_LIMIT - 1)),
+	PLRT_IO = ((uint64_t)1 << (UINT_SHIFT_LIMIT - 2)),
+	PLRT_STRING = ((uint64_t)1 << (UINT_SHIFT_LIMIT - 3)),
+	PLRT_ERRNO = ((uint64_t)1 << (UINT_SHIFT_LIMIT - 4)),
+
+	PLRT_OOM = 1,
+	PLRT_FAILED_ALLOC = 2,
+	PLRT_INVALID_PTR = 3,
+	PLRT_NULL_PTR = 4,
+
+	PLRT_ACCESS = 5,
+	PLRT_INVALID_MODE = 6,
+	PLRT_ALREADY_EXISTS = 7,
+	PLRT_NO_EXIST = 8,
+	PLRT_IOERR = 9,
+
+	PLRT_NOT_PLCHAR = 10,
+	PLRT_NOT_COMPRESSED = 11
+} plrtret_t;
