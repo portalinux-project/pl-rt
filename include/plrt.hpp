4a58651267274387bc2d0835ef6e88c0c5483b9a
pocketlinux32 <pocketlinux32@gmail.com>
2024-03-13 12:08:20 -0500

    nvm forgor cpp headers

--- include/plrt.hpp
+++ include/plrt.hpp
@@ -78,16 +78,16 @@ namespace plRT {
 				plRT::cApi::plptr_t fatPtr;
 				tracker &tracker;
 			public:
-				fatPointer(pl32::cApi::memptr_t pointer, size_t size, tracker &tracker){
-					fatPtr.array = pointer;
+				fatPointer(plRT::cApi::memptr_t pointer, size_t size, tracker &tracker, bool is2dimArray){
+					fatPtr.pointer = pointer;
 					fatPtr.size = size;
 					fatPtr.isMemAlloc = isMemAlloc;
 					fatPtr.mt = tracker.getMTHandle();
 					is2DArray = is2dimArray;
 				}
 
-				fatPointer(pl32::cApi::memptr_t pointer, size_t size, bool is2dimArray){
-					fatPtr.array = pointer;
+				fatPointer(plRT::cApi::memptr_t pointer, size_t size, bool is2dimArray){
+					fatPtr.pointer = pointer;
 					fatPtr.size = size;
 					fatPtr.isMemAlloc = false;
 					fatPtr.mt = NULL;
@@ -96,13 +96,17 @@ namespace plRT {
 
 				~fatPointer(){
 					if(fatPtr.isMemAlloc && fatPtr.mt != NULL)
-						pl32::cApi::plMTFreeArray(&fatPtr, is2DArray);
+						plRT::cApi::plMTFreeArray(&fatPtr, is2DArray);
+				}
+
+				plRT::cApi::memptr_t getRawPointer(){
+					return fatPtr.pointer;
 				}
 		};
 	}
 
-	memory::fatPointer parser(pl32::cApi::string_t input, memory::tracker &tracker){
-		pl32::cApi::plfatptr_t* tempData = pl32::cApi::plParser(input, tracker.getMTHandle());
+	memory::fatPointer parser(plRT::cApi::string_t input, memory::tracker &tracker){
+		plRT::cApi::plfatptr_t* tempData = plRT::cApi::plParser(input, tracker.getMTHandle());
 
 		memory::fatPointer returnPointer(tempData->array, tempData->size, true, true, tracker);
 		tracker.free(tempData);
@@ -113,31 +117,33 @@ namespace plRT {
 	class file {
 		private:
 			memory::tracker tracker;
-			pl32::cApi::plfile_t* fileHandle;
+			plRT::cApi::plfile_t* fileHandle;
 		public:
-			file(pl32::cApi::string_t filename, pl32::cApi::string_t mode){
+			file(plRT::cApi::string_t filename, plRT::cApi::string_t mode){
 				tracker.init(1024 * 1024);
-				fileHandle = pl32::cApi::plFOpen(filename, mode, tracker.getMTHandle());
+				fileHandle = plRT::cApi::plFOpen(filename, mode, tracker.getMTHandle());
 			}
 
 			file(size_t sizeOfBuffer){
 				tracker.init(sizeOfBuffer);
-				fileHandle = pl32::cApi::plFOpen(NULL, NULL, tracker.getMTHandle());
+				fileHandle = plRT::cApi::plFOpen(NULL, NULL, tracker.getMTHandle());
 			}
 
 			~file(){
-				pl32::cApi::plFClose(fileHandle);
+				plRT::cApi::plFClose(fileHandle);
 			}
 
 			memory::fatPointer read(size_t amountOfBytes, memory::tracker &extTracker){
-				pl32::cApi::memptr_t tempPtr = extTracker.alloc(amountOfBytes);
-				pl32::cApi::plFRead(tempPtr, amountOfBytes, 1, fileHandle);
+				plRT::cApi::memptr_t tempPtr = extTracker.alloc(amountOfBytes);
+				plRT::cApi::plFRead(tempPtr, amountOfBytes, 1, fileHandle);
 
 				return memory::fatPointer(tempPtr, amountOfBytes, false, true, extTracker);
 			}
 
 			void write(memory::fatPointer data){
-				pl32::cApi::plFWrite(data.getPointer(), data.getSize(), 1, fileHandle);
+				plRT::cApi::plFWrite(data.getPointer(), data.getSize(), 1, fileHandle);
 			}
 	};
+
+	
 }
